e4162e39b526cc6783c1ba5739b996d863e41bbd
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2025-12-25 21:23:59 +0530

    [ADDED]ishwar: Basics of Loops in Python

---
+++ LoopsOperation .py
@@ -0,0 +1,17 @@
+for i in range(10):
+    print(i)
+
+print("---><---")
+#range(start, stop, step)
+for i in range(1,10,2):
+    print(i)
+
+print("---><---")
+name = "python"
+for i in name:
+    print(i)
+
+print("---><---")
+num = [10,20,30,40,50]
+for i in num:
+    print(i)
