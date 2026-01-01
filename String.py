b97741e8c9bf783994b201e69504f6a01160eef9
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-01-01 23:46:01 +0530

    [ADDED]ishwar : Basic String operations

---
+++ String.py
@@ -0,0 +1,12 @@
+text = "PythonDSA"
+
+print("Original String:", text)
+print("Length:", len(text))
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("First Character:", text[0])
+print("Slice (0:6):", text[0:6])
+print("Reverse String:", text[::-1])
+
+for ch in text:
+    print(ch)
